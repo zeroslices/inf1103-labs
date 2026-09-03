6be530dc94a6c36c5a5a2ea14c62518135c98d02
Yuan Fong <2603051@sit.singaporetech.edu.sg>
2026-09-03 21:38:41 +0800

    Third Post

--- hello.py
+++ hello.py
@@ -11,3 +11,14 @@ followers = 100
 print("Username:", username)
 print("Bio:", bio)
 print("Followers:", followers)
+
+followers = 100
+
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers += 10
+print("Day 3:", followers)
