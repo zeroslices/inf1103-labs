ba147a75a4db4dcdc65e2ce9fbe9191ad33f348a
Yuan Fong <2603051@sit.singaporetech.edu.sg>
2026-09-03 21:36:14 +0800

    Second Post

--- hello.py
+++ hello.py
@@ -2,3 +2,12 @@ print("=================================")
 print("Welcome here")
 print("My first post!")
 print("=================================")
+
+
+username = "cool_creater"
+bio = "Fun Blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
