78ae3536d8412d8413fff466b9c622448ef43cd0
Yuan Fong <2603051@sit.singaporetech.edu.sg>
2026-09-04 14:50:01 +0800

    Fifth Activity

--- hello.py
+++ hello.py
@@ -24,7 +24,7 @@ followers += 10
 print("Day 3:", followers)
 
 username = input("Enter Username: ")
-age = input("Enter Age: ")
+age = int(input("Enter Age: "))
 category = input("Enter Content Category: ")
 
 print("\nInstagram Profile")
@@ -32,3 +32,6 @@ print("=================================")
 print("Username:", username)
 print("Age:", age)
 print("Category:", category)
+
+if age > 40 and category == "fun":
+    print("You are old what is fun for you??")
